002d2445d0e6a5595b1dd8f011d7894ab133affc
Zainab Zahid <108534854+zainabzahid711@users.noreply.github.com>
2025-03-29 16:10:35 +0500

    connect with front end

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,20 @@
-export default [
-  'strapi::logger',
+// Keep only this (delete the other export):
+module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000',
+        'https://demo-project-gamma-tawny.vercel.app'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+    },
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
