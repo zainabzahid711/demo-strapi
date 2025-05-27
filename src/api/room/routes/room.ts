ad48cf65ccec61d9b02df3c635d7af5fffa43306
Zainab Zahid <108534854+zainabzahid711@users.noreply.github.com>
2025-05-27 23:31:55 +0500

    database schema updated to rooms

---
+++ src/api/room/routes/room.ts
@@ -0,0 +1,7 @@
+/**
+ * room router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::room.room');
---
+++ src/api/room/services/room.ts
@@ -0,0 +1,7 @@
+/**
+ * room service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::room.room');
